384175a1b32f766325c3b90b3752534666c0a0a4
Guixaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-07-01 17:14:40 -0300

    Aprofundando em Models no DJANGO

---
+++ projeto1_treino/back/lerning_logs/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from lerning_logs.models import Topic # importando da pasta da nossa aplicação do arquivo models a Class Topic
+
+# Registre o seu Model aqui
+
+admin.site.register(Topic) # Registrando a nossa Class Topic
+
+
---
+++ projeto1_treino/back/lerning_logs/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+
+# Crie os seus Models (Entidades do Banco de dados) aqui
+
+class Topic(models.Model): # fazendo uma herança 
+    
+    # definindo um atributo que será do tipo CharField com no máximo 200 caracteres, basicamente representa o nosso VARCHAR(200)
+    text = models.CharField(max_length=200) 
+    
+    # # definindo um atributo da nossa classe que vai automaticamente adicionar data e hora do nosso computador neste campo
+    date_added = models.DateTimeField(auto_now_add=True) 
+    
+    # crinado o nosso contrutor (opcional, não é obrigatório)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) # atributo especial que chama o construtor da class pai
+    
+    '''
+        Anotações sobre *args e **kwargs
+        
+        *args:
+            
+            É uma convenção, não uma palavra reservada do Python 
+            (você poderia usar outro nome, mas é padrão usar args).
+
+            O asterisco * significa: receber um número variável de argumentos 
+            posicionais como uma tupla.
+
+        **kwargs:
+            
+            Também uma convenção (significa “keyword arguments”).
+
+            O ** significa: receber um número variável de argumentos nomeados 
+            (chave=valor) como um dicionário.
+    
+    '''
+    
+    # criando um método para organizar o nosso administrativo. 
+    def __str__(self):
+        """ Devolve uma representação em string do Modelo. """
+        return self.text # funciona como um this do java, basicamente vamos referenciar ao proprio atributo text
+    
+    
+class Entry(models.Model):
+
+    # criando atributos, lembre-se que é opcional criar um construtor
+    
+    # puxando a FOREIGN KEY da ENTIDADE TOPIC, ligação entre ENTRY E TOPIC
+    topic = models.ForeignKey(Topic, on_delete = models.deletion) # Entry vai ser deletado junto com o atributo pai
+    annotations = models.TextField() # valor ilimitado de caracteres
+
+    def __str__(self):
+        return self.topic, self.annotations
